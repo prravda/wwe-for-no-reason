572b62e291e4e28a3ef23c714761cf02812dc4f9
prravda <sonofhuman20@hufs.ac.kr>
2024-12-27 23:45:34 +0900

    phase: TCP handler with goroutine and multiple TCP requests

---
+++ echo-server/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+
+	for i := 0; i < 100_000; i++ {
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(conn, "Hello, Server\n")
+
+		status, err := bufio.NewReader(conn).ReadString('\n')
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Message from server: %s", status)
+	}
+}
---
+++ echo-server/tcp_go/tcp_server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serveing %s\n", c.RemoteAddr().String())
+
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed")
+				break
+			}
+			log.Fatal(err)
+			return
+		}
+
+		temp := strings.TrimSpace(netData)
+
+		c.Write([]byte(temp + "\n"))
+	}
+
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConnection(conn)
+	}
+}
